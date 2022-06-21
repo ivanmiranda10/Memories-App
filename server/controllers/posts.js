418d02b80b677b5684a834563903215c075e4c4f
Ivan Miranda <gonzalobarnichea10@gmail.com>
2022-06-21 00:37:06 -0300

    Update Controller done

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const PostMessage = require("../models/postMessage");
 
 const getPosts = async (req, res) => {
@@ -30,4 +31,19 @@ const createPost = (req, res) => {
   }
 };
 
-module.exports = { getPosts, createPost };
+const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("No post with that id");
+  }
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true }
+  );
+  res.status(201).json(updatedPost);
+};
+
+module.exports = { getPosts, createPost, updatePost };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getPosts, createPost } = require("../controllers/posts");
+const { getPosts, createPost, updatePost } = require("../controllers/posts");
 
 router.get("/posts", getPosts);
 router.post("/post/add", createPost);
+router.put("/post/update/:id", updatePost);
 
 module.exports = router;
